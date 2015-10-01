f60bd269d9922ff86ef39d1ae1aeddea0f1d9c20
jgollner <jamesgollner@gmail.com>
2015-10-01 12:38:26 -0400

    NewProgress

--- _site/js/script.js
+++ _site/js/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
   $('#landing').particleground({
-    dotColor: '#33393d',
-    lineColor: '#33393d'
+    dotColor: '#2a2a2a',
+    lineColor: '#2a2a2a',
+    density: '7000'
   });
   $('.intro').css({
     'margin-top': -($('.intro').height() / 2)
   });
 });
+
